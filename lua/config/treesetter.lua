e94e37c35ff459249e0d82a2f73fde1fa893d648
ronniedroid <ronni.n.yousif@gmail.com>
2021-06-15 18:23:48 +0300

    fixed an error with language/context aware commenting through treesetter

--- lua/config/treesetter.lua
+++ lua/config/treesetter.lua
@@ -4,25 +4,6 @@ require "nvim-treesitter.configs".setup {
     enable = true
   },
   context_commentstring = {
-    enable = true,
-    config = {
-      -- Languages that have a single comment style
-      typescript = "// %s",
-      css = "/* %s */",
-      scss = "/* %s */",
-      php = "// %s",
-      html = "<!-- %s -->",
-      svelte = "<!-- %s -->",
-      lua = "-- %s",
-      vue = {
-        __default = "<!-- %s -->",
-        template = "<!-- %s -->",
-        html = "<!-- %s -->",
-        javascript = "// %s",
-        script = "// %s",
-        css = "/* %s */",
-        comment = "<!-- %s -->"
-      }
-    }
+    enable = true
   }
 }
